9e0a8e4e711dd2cc38d53fa3ea5bf8465cbae5a6
Ross Cameron <2659628c@student.gla.ac.uk>
2023-03-04 22:45:11 +0000

    login page and removed comments from base as authentication works

--- gsr/urls.py
+++ gsr/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('test/categoryform/', views.test_category_form, name='test_category_form'),
     path('test/shopform/', views.test_shop_form, name='test_shop_form'),
-    path('signup/',views.signup,name = 'signup')
+    path('signup/',views.user_signup,name = 'signup'),
+    path('login/', views.user_login, name='login')
 ]
--- gsr/views.py
+++ gsr/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 from gsr.forms import CategoryForm, ShopForm, UserForm
 
@@ -42,7 +44,7 @@ def test_shop_form(request: HttpRequest) -> HttpResponse:
 
     return render(request, 'gsr/test/shopform.html', context={'form': form, 'success': success})
 
-def signup(request):
+def user_signup(request):
     """TODO: Need to change the groups input probably to a boolean of is shop owner
     or not shop owner"""
     registered = False
@@ -61,5 +63,26 @@ def signup(request):
         user_form = UserForm()
 
     return render(request,'gsr/signup.html',context={'user_form':user_form,'registered':registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+
+                login(request, user)
+                return redirect(reverse('gsr:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'gsr/login.html')
 
 
