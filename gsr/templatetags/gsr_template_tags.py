3c2b2aa5121c009c7f6bf2ee1e90a04ec9f066f9
DanielHally <2660097h@student.gla.ac.uk>
2023-03-15 10:25:00 +0000

    selected template tag

--- gsr/templatetags/gsr_template_tags.py
+++ gsr/templatetags/gsr_template_tags.py
@@ -6,4 +6,12 @@ register = template.Library()
 
 @register.inclusion_tag('gsr/stars.html')
 def star_rating(shop: Shop, method: str = Shop.RatingMethod.OVERALL_RATING):
+    """Outputs the star rating for a shop"""
+
     return { 'stars' : range(Shop.RatingMethod.methods[method](shop)) }
+
+@register.inclusion_tag('gsr/selected.html')
+def selected(item_name: str, default_name: str):
+    """Outputs `selected="selected"` if the names match"""
+
+    return { 'selected' : item_name == default_name }
