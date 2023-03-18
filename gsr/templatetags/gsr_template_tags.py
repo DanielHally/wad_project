000ca64d4a0d26a80c8900e22ca1c27b09c471bb
DanielHally <2660097h@student.gla.ac.uk>
2023-03-18 13:04:19 +0000

    Move template tags to folder

--- gsr/templatetags/gsr_template_tags.py
+++ gsr/templatetags/gsr_template_tags.py
@@ -4,7 +4,7 @@ from gsr.models import Review, Shop
 
 register = template.Library()
 
-@register.inclusion_tag('gsr/stars.html')
+@register.inclusion_tag('gsr/tags/stars.html')
 def star_rating(shop: Shop, method: str = Shop.RatingMethod.OVERALL_RATING):
     """Outputs the star rating for a shop"""
 
@@ -14,7 +14,7 @@ def star_rating(shop: Shop, method: str = Shop.RatingMethod.OVERALL_RATING):
         'greystars' : range(Review.STARS_MAX - stars),
     }
 
-@register.inclusion_tag('gsr/selected.html')
+@register.inclusion_tag('gsr/tags/selected.html')
 def selected(item_name: str, default_name: str):
     """Outputs `selected="selected"` if the names match"""
 
