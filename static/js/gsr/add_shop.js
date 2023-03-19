c0109f5755fc8142a9ce270ad08cd4b0a37aa254
DanielHally <2660097h@student.gla.ac.uk>
2023-03-19 15:09:20 +0000

    Format add_shop.js

--- static/js/gsr/add_shop.js
+++ static/js/gsr/add_shop.js
@@ -15,16 +15,16 @@ const NEXT_INPUT = "#next"
 /*
     Setup hidden next input on page load
 */
-$(document).ready(()=>{
-		  $('#image_field').change(function(){
-			const file = this.files[0];
-			if (file){
-			  let reader = new FileReader();
-			  reader.onload = function(event){
-				$('#imgPreview').css('backgroundImage', 'url(' + event.target.result + ')');
-				$('#imgPreview').html('');
-			  }
-			  reader.readAsDataURL(file);
-			}
-		  });
-		});
+$(document).ready(() => {
+    $('#image_field').change(function () {
+        const file = this.files[0];
+        if (file) {
+            let reader = new FileReader();
+            reader.onload = function (event) {
+                $('#imgPreview').css('backgroundImage', 'url(' + event.target.result + ')');
+                $('#imgPreview').html('');
+            }
+            reader.readAsDataURL(file);
+        }
+    });
+});
