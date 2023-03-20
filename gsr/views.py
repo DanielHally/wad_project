7a439fa5f446a84ce6be0b08ccb3c30b95ea340e
DanielHally <2660097h@student.gla.ac.uk>
2023-03-20 12:40:11 +0000

    Remove category form test

--- gsr/views.py
+++ gsr/views.py
@@ -3,29 +3,13 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required
 from django.db.models import Avg
 from gsr.models import Category, RatedModel, Shop, Review
 from gsr.forms import CategoryForm, ShopForm, UserForm
 from django.contrib.staticfiles.templatetags.staticfiles import static
 
 # Create your views here.
-def test_category_form(request: HttpRequest) -> HttpResponse:
-    """A page to test CategoryForm, not for use in final site"""
-
-    success = False
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-        if form.is_valid():
-            form.save()
-            success = True
-        else:
-            print(form.errors)
-    else:
-        form = CategoryForm()
-
-    return render(request, 'gsr/test/categoryform.html', context={'form': form, 'success': success})
-
 
 def user_signup(request):
     """TODO: Need to change the groups input probably to a boolean of is shop owner
