2d31958e06363b0b9ee3f7898901c2d8436e3c7d
DanielHally <2660097h@student.gla.ac.uk>
2023-03-23 19:56:25 +0000

    Formatting

--- static/js/gsr/view_shop.js
+++ static/js/gsr/view_shop.js
@@ -10,55 +10,51 @@ $(document).ready(function() {
     }
 
     $('.reply-button').click(function() {
-      const buttonText = $(this).text();
-      const reviewId = $(this).data('review-id');
-      if (buttonText === "Reply") {
-        $(this).text("Close");
-      }else {
-        $(this).text("Reply");
-      }
-      $(`#reply-form-${reviewId}`).toggle();
+        const buttonText = $(this).text();
+        const reviewId = $(this).data('review-id');
+        if (buttonText === "Reply") {
+            $(this).text("Close");
+        }else {
+            $(this).text("Reply");
+        }
+        $(`#reply-form-${reviewId}`).toggle();
+    });
 
+    $('.show-replies').click(function(){
+        const reviewId = $(this).data('review-id');
+        const buttonText = $(this).text();
 
-  });
-
-$('.show-replies').click(function(){
-    const reviewId = $(this).data('review-id');
-    const buttonText = $(this).text();
-
-    if(buttonText === "Show replies") {
-        $(this).text("Hide replies");
-        $.ajax({
-            type: 'GET',
-            url: 'show_replies/',
-            data: {
-              review_id: reviewId,
-            },
-            success: function(data){
-                const replies = data.replies;
-                $('#replies-section-' + reviewId).empty();
-                $.each(replies, function(index, reply) {
-                    const username = reply[0];
-                    const comment = reply[1].replace('\n', '<br/>');
-                    const replyText = '<div class="row">' +
-                                          '<div class="col-md-1"></div>' +
-                                              '<div class="col-md-10">' +
-                                                  '<strong>' + username + '</strong>' +
-                                                  '<p>' + comment + '</p>' + '<hr>'
-                                              '</div>' +
-                                          '</div>' +
-                                      '</div>';
-                    $('#replies-section-' + reviewId).append(replyText);
-                });
-            }
-        });
-    } else {
-        $(this).text("Show replies");
-    }
-    $(`#replies-section-${reviewId}`).toggle()
-});
-
-
+        if(buttonText === "Show replies") {
+            $(this).text("Hide replies");
+            $.ajax({
+                type: 'GET',
+                url: 'show_replies/',
+                data: {
+                review_id: reviewId,
+                },
+                success: function(data){
+                    const replies = data.replies;
+                    $('#replies-section-' + reviewId).empty();
+                    $.each(replies, function(index, reply) {
+                        const username = reply[0];
+                        const comment = reply[1].replace('\n', '<br/>');
+                        const replyText = '<div class="row">' +
+                                            '<div class="col-md-1"></div>' +
+                                                '<div class="col-md-10">' +
+                                                    '<strong>' + username + '</strong>' +
+                                                    '<p>' + comment + '</p>' + '<hr>'
+                                                '</div>' +
+                                            '</div>' +
+                                        '</div>';
+                        $('#replies-section-' + reviewId).append(replyText);
+                    });
+                }
+            });
+        } else {
+            $(this).text("Show replies");
+        }
+        $(`#replies-section-${reviewId}`).toggle()
+    });
 });
 
 
